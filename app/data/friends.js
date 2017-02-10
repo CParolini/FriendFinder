4786027c400e307a5bdd4029a4e863c5acce7fe9
Chris Parolini <cparolini@cparolini-mbp1.ip-soft.net>
2017-02-10 15:08:43 -0600

    adding home screen HTML

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,64 +1,67 @@
-var applicants = [
-  {
-  "name":"Tony Romo",
-  "photo":"https://insidethestar.com/wp-content/uploads/2016/04/happy-birthday-to-dallas-cowboys-quarterback-tony-romo-5.jpg",
-  "scores":[
-     4,
-     3,
-     1,
-     4,
-     2,
-     5,
-     2,
-     1,
-     3,
-     2]
-},
-{
-  "name":"Dez Bryant",
-  "photo":"http://images.performgroup.com/di/library/sporting_news/b9/de/dez-bryant-050714-getty-ftrjpg_1hg1qe7f29q2e1mp7902d5hqxi.jpg?t=816894125",
-  "scores":[
-     1,
-     5,
-     2,
-     4,
-     5,
-     2,
-     1,
-     3,
-     4,
-     5]
-},
-{
-  "name":"Cole Beasley",
-  "photo":"https://heavyeditorial.files.wordpress.com/2015/08/gettyimages-460644986.jpg?quality=65&strip=all&w=780&strip=all",
-  "scores":[
-     5,
-     4,
-     3,
-     2,
-     1,
-     5,
-     5,
-     3,
-     2,
-     1]
-},
-{
-  "name":"Ezekiel Elliott",
-  "photo":"http://l.yimg.com/ny/api/res/1.2/F_a7EF7VMZFnySQPbzakjg--/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/http://media.zenfs.com/en/homerun/feed_manager_auto_publish_494/b68628774e3c49a3be72d024c68b94ad",
-  "scores":[
-     5,
-     4,
-     3,
-     2,
-     1,
-     5,
-     5,
-     3,
-     2,
-     1]
-},
+var applicants = [{
+        "name": "Tony Romo",
+        "photo": "https://insidethestar.com/wp-content/uploads/2016/04/happy-birthday-to-dallas-cowboys-quarterback-tony-romo-5.jpg",
+        "scores": [
+            4,
+            3,
+            1,
+            4,
+            2,
+            5,
+            2,
+            1,
+            3,
+            2
+        ]
+    },
+    {
+        "name": "Dez Bryant",
+        "photo": "http://images.performgroup.com/di/library/sporting_news/b9/de/dez-bryant-050714-getty-ftrjpg_1hg1qe7f29q2e1mp7902d5hqxi.jpg?t=816894125",
+        "scores": [
+            1,
+            5,
+            2,
+            4,
+            5,
+            2,
+            1,
+            3,
+            4,
+            5
+        ]
+    },
+    {
+        "name": "Cole Beasley",
+        "photo": "https://heavyeditorial.files.wordpress.com/2015/08/gettyimages-460644986.jpg?quality=65&strip=all&w=780&strip=all",
+        "scores": [
+            5,
+            4,
+            3,
+            2,
+            1,
+            5,
+            5,
+            3,
+            2,
+            1
+        ]
+    },
+    {
+        "name": "Ezekiel Elliott",
+        "photo": "http://l.yimg.com/ny/api/res/1.2/F_a7EF7VMZFnySQPbzakjg--/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/http://media.zenfs.com/en/homerun/feed_manager_auto_publish_494/b68628774e3c49a3be72d024c68b94ad",
+        "scores": [
+            5,
+            4,
+            3,
+            2,
+            1,
+            5,
+            5,
+            3,
+            2,
+            1
+        ]
+    },
 ]
 
 module.exports = applicants;
