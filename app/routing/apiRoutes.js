9c60583881c0aa4a343186a9bf5c7116370ab8bc
Chris Parolini <cparolini@cparolini-mbp1.ip-soft.net>
2017-02-10 15:41:11 -0600

    adding html and api routes

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,32 @@
+//required files
+var friends = require('../data/friends.js');
+var path = require('path');
+
+//module export function
+module.exports = function(app) {
+    //Read information from the friends.js
+    app.get('/api/friends', function(req, res) {
+        //convert information into JSON form
+        res.json(friends);
+    });
+    //Create the information and return it to JSON form
+    //Push informations into the friends.js file
+    //this function calculates the users score and matches with a friend in the database
+    app.post('/api/friends', function(req, res) {
+        var totalDifference = 100;
+        var yourMatch;
+        friends.forEach(function(item) {
+            var newDifference = 0;
+            for (i = 0; i < item.scores.length; i++) {
+                newDifference += Math.abs(item.scores[i] - req.body.scores[i]);
+            }
+            if (newDifference <= totalDifference) {
+                totalDifference = newDifference;
+                theMatch = item;
+            }
+        });
+        res.json(theMatch);
+        friends.push(req.body);
+        res.json(true);
+    });
+};
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,14 @@
+//required modules to be used
+var path = require("path");
+var bodyParser = require('body-parser');
+//module.exports allows the information to be used by other files
+module.exports = function(app) {
+    // HTML GET Requests
+    app.get("/survey", function(req, res) {
+        res.sendFile(path.join(__dirname, "/../public/survey.html"));
+    });
+    // If no matching route is found default to home
+    app.use(function(req, res) {
+        res.sendFile(path.join(__dirname, "/../public/home.html"));
+    });
+};
